b67c740a3c9082e9c4b36c7c1a70a7c31d2a59a9
em4715 <em4715@naver.com>
2022-08-01 03:29:23 +0900

    detail(홈, 메뉴, 리뷰부분) 1차 수정, 클릭 후에도 hover 적용되도록 수정 필요

--- src/components/detail.js
+++ src/components/detail.js
@@ -5,95 +5,68 @@ import DetailMenu from "./DetailPages/DetailMenu";
 import DetailReview from "./DetailPages/DetailReview";
 import ScrollBtn from "./ScrollBtn";
 import ReviewBtn from "./ReviewBtn";
-import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux"
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { DetailCafeBanner } from "../redux/modules/AllSlice";
 import DetailBanner from "./DetailPages/DetailBenner";
 
+import "../css/Detail.css";
+
 const Detail = () => {
+  const parm = useParams();
+  const dispatch = useDispatch();
+  //parm에 카페아이디 주세요]
+  React.useEffect(() => {
+    dispatch(DetailCafeBanner(parm.id));
+  }, [dispatch, parm.id]);
 
-    const parm = useParams();
-    const dispatch = useDispatch();
-    //parm에 카페아이디 주세요]
-    React.useEffect(()=>{
-        dispatch(DetailCafeBanner(parm.id))
-    },[dispatch, parm.id])
+  const list = useSelector((state) => state.AllSlice.DetailCafeBanner);
+  console.log(list?.imageList);
+  console.log(parm["*"]);
+  const [Menu, setMenu] = useState(parm["*"] === "review" ? "C" : "A");
 
-    const list = useSelector((state) => state.AllSlice.DetailCafeBanner);
-    console.log(list?.imageList)
-    console.log(parm["*"])
-    const [Menu, setMenu] = useState(parm["*"]==="review"?"C":"A")
+  return (
+    <>
+      <DetailBanner image={list?.imageList} />
+      {/*클릭 후에도 hover 값이 동일하게 적용되는게 필요합니다!!*/}
+      <Container>
+        <div className="CategoryTotal">
+          <div className="Category">
+            <a
+              style={{ marginRight: "200px" }}
+              onClick={() => {
+                setMenu("A");
+              }}>
+              홈
+            </a>
+            <a
+              style={{ marginRight: "200px" }}
+              onClick={() => {
+                setMenu("B");
+              }}>
+              메뉴
+            </a>
+            <a
+              onClick={() => {
+                setMenu("C");
+              }}>
+              리뷰
+            </a>
+          </div>
 
-    return (
-        <>
-            <DetailBanner image = {list?.imageList} />
-            <Container>
-                <div>
-                    <CategoryBox
-                    onClick={()=>{setMenu("A")}}>
-                    홈
-                    </CategoryBox>
-                    <CategoryBox
-                    onClick={()=>{setMenu("B")}}>
-                    메뉴
-                    </CategoryBox>
-                    <CategoryBox
-                    onClick={()=>{setMenu("C")}}>
-                    리뷰
-                    </CategoryBox>
-                </div>
-            </Container>
-            <div>
-                {
-                Menu === "A" && <DetailHome cafeid = {parm} /> ||
-                Menu === "B" && <DetailMenu cafeid = {parm} /> ||
-                Menu === "C" && <DetailReview cafeid = {parm} />
-            }
-            </div>
-            <ReviewBtn/>
-            {/* <ScrollBtn/> */}
-        </>
-    );
-}
-const Container = styled.div`
-    width: 1000px;
-    height: 100%;
+          <div className="CategoryLine"></div>
+        </div>
+      </Container>
+      <div>
+        {(Menu === "A" && <DetailHome cafeid={parm} />) ||
+          (Menu === "B" && <DetailMenu cafeid={parm} />) ||
+          (Menu === "C" && <DetailReview cafeid={parm} />)}
+      </div>
+      <ReviewBtn />
+    </>
+  );
+};
 
-    margin: 0px auto;
-    margin-top: 70px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-
-    position: relative;
-    border-bottom: solid 1px #D9D9D9;
-    
-    & button {
-        width: 224px;
-        height: 34px;
-        font-size: 32px;
-        margin-right: 40px;
-        font-family: 'Arita-dotum-Medium';
-        
-    }
-
-    & button:hover {
-        text-decoration: underline;
-    }
-`;
-
-const CategoryBox = styled.button`
-
-    border: 1px solid #D9D9D9;
-    font-weight: bold;
-    background-color: #D9D9D9;
-    cursor: pointer;
-    color: gray;
-    
-    &: hover {
-        color: #19221F;
-    }
-`;
+const Container = styled.div``;
 
 export default Detail;
